c17466254fc598390f5b946200de34ce38154745
jonnieey <johnjahi55@gmail.com>
2025-04-01 23:08:27 +0300

    feat: Add config for private windows. Add bookmarks

--- .config/qutebrowser/config-priv.py
+++ .config/qutebrowser/config-priv.py
@@ -1,6 +1,23 @@
-c.hints.selectors['all'] += ['label.upload-step', 'div.radio-icon', 'div.check-icon']
-c.fileselect.multiple_files.command = ["st", "-e", "clifm", "--sel-file={}", "~/.local/share/transcriptor4/clients" ]
-c.fileselect.single_file.command = ["st", "-e", "clifm",  "--sel-file={}", "~/.local/share/transcriptor4/clients" ]
+c.hints.selectors["all"] += [
+    "label.upload-step",
+    "div.radio-icon",
+    "div.check-icon",
+    "div.btn",
+]
+c.fileselect.multiple_files.command = [
+    "st",
+    "-e",
+    "clifm",
+    "--sel-file={}",
+    "~/.local/share/transcriptor4/clients",
+]
+c.fileselect.single_file.command = [
+    "st",
+    "-e",
+    "clifm",
+    "--sel-file={}",
+    "~/.local/share/transcriptor4/clients",
+]
 config.source("common.py")
 config.source("binds.py")
 config.source("theme.py")
